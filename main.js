2c8aecf8335fef79567edbf952517f8dfe8bedd3
kaitok44 <kaito.2151@gmail.com>
2024-04-28 22:20:29 +0900

    code formatted

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
 const container = document.querySelector(".container");
-const clearButton =  document.querySelector(".clear-button");
+const clearButton = document.querySelector(".clear-button");
 
 // generating random color
-function setRandomColor(){
-    let color = '#'+Math.floor(Math.random()*16777215).toString(16);
-    return color;
+function setRandomColor() {
+  let color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  return color;
 }
 
 //create grid
-function createGrid(gridNum){
-    container.replaceChildren();
-    for (let i = 0; i < gridNum; i++){
-        const row = document.createElement("div");
-        row.classList.add("grid-row");
+function createGrid(gridNum) {
+  container.replaceChildren();
+  for (let i = 0; i < gridNum; i++) {
+    const row = document.createElement("div");
+    row.classList.add("grid-row");
 
-     for (let j = 0; j < gridNum; j++){
-        const widthAndHeight = 960 / gridNum;
-        const gridBox = document.createElement("div");
-        gridBox.classList.add("grid-box")
-        gridBox.style.width = `${widthAndHeight}px`;
-        gridBox.style.height = `${widthAndHeight}px`;
-        gridBox.addEventListener("mouseover",() => {
-            gridBox.style.backgroundColor = setRandomColor();
-        })
-        row.appendChild(gridBox);
-     }
-    container.appendChild(row);
+    for (let j = 0; j < gridNum; j++) {
+      const widthAndHeight = 960 / gridNum;
+      const gridBox = document.createElement("div");
+      gridBox.classList.add("grid-box");
+      gridBox.style.width = `${widthAndHeight}px`;
+      gridBox.style.height = `${widthAndHeight}px`;
+      gridBox.addEventListener("mouseover", () => {
+        gridBox.style.backgroundColor = setRandomColor();
+      });
+      row.appendChild(gridBox);
     }
+    container.appendChild(row);
+  }
 }
 createGrid(16);
 
- clearButton.addEventListener("click", () => {
-    let userSize = Number(prompt("set your grid size"));
-    
-    while  (userSize > 100){
+clearButton.addEventListener("click", () => {
+  let userSize = Number(prompt("set your grid size"));
+
+  while (userSize > 100) {
     userSize = Number(prompt("ERROR: pick a number 100 or less"));
-    }
-createGrid(userSize);
-})
+  }
+  createGrid(userSize);
+});
